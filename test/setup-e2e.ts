07a40538d9ae96e1d3a972c96f2e9897aee32d71
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-08-28 14:22:04 -0300

    chore: config Test e2e

---
+++ test/setup-e2e.ts
@@ -0,0 +1,47 @@
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
+
+import { PrismaClient } from '@prisma/client'
+import { config } from 'dotenv'
+
+import { envSchema } from '@/env/env'
+
+config({
+  path: './.env',
+  override: true,
+})
+config({
+  path: './.env.test',
+  override: true,
+})
+
+const env = envSchema.parse(process.env)
+
+const prisma = new PrismaClient()
+
+function generateUniqueDatabaseURL(schemaId: string) {
+  if (!env.DATABASE_URL) {
+    throw new Error('DATABASE_URL environment variable is required')
+  }
+  const url = new URL(env.DATABASE_URL)
+  url.searchParams.set('schema', schemaId)
+
+  return url.toString()
+}
+
+const DatabaseUUID = randomUUID()
+
+beforeAll(async () => {
+  const databaseURL = generateUniqueDatabaseURL(DatabaseUUID)
+  process.env.DATABASE_URL = databaseURL
+
+  execSync('pnpm prisma migrate deploy')
+})
+
+afterAll(async () => {
+  await prisma.$executeRawUnsafe(
+    `DROP SCHEMA IF EXISTS "${DatabaseUUID}" CASCADE`,
+  )
+
+  await prisma.$disconnect()
+})
