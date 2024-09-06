4aeeab800df3b9d639e291966e23160930ec23bc
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-09-06 09:35:21 -0300

    refactor: add id and remove  updatedAt

--- src/infra/http/presenters/task-presenter.ts
+++ src/infra/http/presenters/task-presenter.ts
@@ -3,11 +3,11 @@ import type { Task } from '@/domain/manager-tasks/enterprise/task'
 export class TaskPresenter {
   static toHTTP(task: Task) {
     return {
+      id: task.id.toString(),
       title: task.title,
       description: task.description,
       completionDate: task.completionDate,
       createdAt: task.createdAt,
-      updatedAt: task.updatedAt,
     }
   }
 }
