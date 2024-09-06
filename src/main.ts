c07f6230f0f12c2c5a237306bd52b66f2a982047
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-09-06 09:34:07 -0300

    feat: add config cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core'
+import * as cors from 'cors'
 
 import { AppModule } from './app.module'
 import { EnvService } from './env/env.service'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.use(
+    cors({
+      origin: 'http://localhost:5173',
+      methods: 'GET,POST,PUT,DELETE,PATCH',
+      allowedHeaders: 'Content-Type,Authorization',
+    }),
+  )
   const envService = app.get(EnvService)
   const port = envService.get('PORT')
   await app.listen(port)
